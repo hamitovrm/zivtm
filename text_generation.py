374e7f25128004f774b45812149635908e0fbf91
hrm <hamitov@gmail.com>
2025-01-29 13:44:53 +0300

    text_generation.py added

---
+++ text_generation.py
@@ -0,0 +1,26 @@
+from huggingface_hub import InferenceClient
+import streamlit as st
+
+client = InferenceClient(
+	provider="hf-inference",
+	api_key="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"
+)
+
+
+
+messages = [
+	{
+		"role": "user",
+		"content": "What is the capital of France?"
+	}
+]
+
+completion = client.chat.completions.create(
+    model="mistralai/Mistral-Nemo-Instruct-2407", 
+	messages=messages, 
+	max_tokens=500
+)
+
+
+st.title('Генерация текста')
+st.write(f"Текст: {сompletion.choices[0].message}")
